01742ed916454b40decdbbe5b5a9fabffff7dafa
kinetas <kimjh7302@naver.com>
2026-05-08 17:11:54 +0900

    1차 개편

--- cardforge/scripts/run_combination_tests.ps1
+++
@@ -1,6 +0,0 @@
-# combination_engine 단위 테스트 실행 (PATH에 flutter 없을 때도 동작)
-$ErrorActionPreference = "Stop"
-Set-Location $PSScriptRoot\..
-. "$PSScriptRoot\flutter_env.ps1"
-flutter pub get
-flutter test test/combination_engine_test.dart
